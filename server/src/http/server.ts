2044bd61ce7536a3f41c2c8cfbdb6ebc01c86fa6
Bruno Vinícius <vbruno96@gmail.com>
2024-09-09 22:57:43 -0300

    feat: create goal route

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,8 +1,38 @@
 import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoal } from '../functions/create-goal'
+import z from 'zod'
 
 const port = 6969
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { desiredWeeklyFrequency, title } = request.body
+
+    await createGoal({
+      title: title,
+      desiredWeeklyFrequency: desiredWeeklyFrequency,
+    })
+  }
+)
 
 app
   .listen({
