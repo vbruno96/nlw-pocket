d329586673fa80c297bd0c71de753bb00445e393
Bruno Vinícius <vbruno96@gmail.com>
2024-09-10 23:05:14 -0300

    fix: tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*/.{ts, tsx}'
-  ],
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'system-ui']
-      }
+        sans: ['Inter', 'system-ui'],
+      },
     },
   },
   plugins: [],
 }
-
