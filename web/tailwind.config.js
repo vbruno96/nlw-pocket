c5c7c7e72e933e884f3d4af0a06cd412b9b5a057
Bruno Vinícius <vbruno96@gmail.com>
2024-09-10 22:37:56 -0300

    chore: add font inter

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     './src/**/*/.{ts, tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui']
+      }
+    },
   },
   plugins: [],
 }
