e073850b3c49906509039ee04d5408238cae50c9
Bruno Vinícius <vbruno96@gmail.com>
2024-09-10 22:12:37 -0300

    build: configure fastify cors

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -8,10 +8,15 @@ import { createGoalRoute } from './routes/create-goal'
 import { createCompletionRoute } from './routes/create-completion'
 import { getPendingGoalsRoute } from './routes/get-pending-goal'
 import { getWeekSummaryRoute } from './routes/get-week-summary'
+import fastifyCors from '@fastify/cors'
 
 const port = 6969
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
